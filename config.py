30d53c920a173ab5ddfd3d15f9d4b951c8972196
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-11-14 13:39:57 +0200

    added more params to config.py

--- config.py
+++ config.py
@@ -1,3 +1,25 @@
 import os
+
+
 PICKLE_OBJ_PATH = os.path.join("Utils","preprocess","pickle_obj")
-PRODUCE_PICKLE_FILES = True
+if not os.path.exists(PICKLE_OBJ_PATH):
+    os.makedirs(PICKLE_OBJ_PATH)
+
+TRAINING = True
+
+LABEL = "kg"
+
+SAVED_MODEL_PATH = os.path.join("models","xgboost")
+if not os.path.exists(SAVED_MODEL_PATH):
+    os.makedirs(SAVED_MODEL_PATH)
+
+
+
+PREPROCESS_DICT = {"lat400sqkm": "scale",
+                  "lon400sqkm": "scale",
+                  "gear": "LabelEncoder",
+                  "year": "scale",
+                  "kg": "scale",
+                  "rate": "scale",
+                  "species": "LabelEncoder",
+                  "sid": "LabelEncoder"}
