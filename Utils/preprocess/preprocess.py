7b2eebbdb792c312e1e211ea990afbfa14e31e3b
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-11-14 13:39:03 +0200

    added train_test_split to preprocess

--- Utils/preprocess/preprocess.py
+++ Utils/preprocess/preprocess.py
@@ -6,52 +6,71 @@ import config
 # lat400sqkm	lon400sqkm	gear	year	kg	     rate	   species	 sid
 # Numirc        Numirc      Encode  Numirc  Numirc   Numirc    Encode    Encode
 PICKLE_OBJ_PATH = config.PICKLE_OBJ_PATH
-PRODUCE_PICKLE_FILES = config.PRODUCE_PICKLE_FILES
-preprocess_map = {"lat400sqkm": "scale",
-                "lon400sqkm":"scale",
-                "gear":"LabelEncoder",
-                "year":"scale",
-                "kg":"scale",
-                "rate":"scale",
-                "species":"LabelEncoder",
-                "sid":"LabelEncoder"}
+TRAINING = config.TRAINING
+LABEL = config.LABEL
+
+preprocess_map = config.PREPROCESS_DICT
 
 class preprocess_fishiries():
-    def __init__(self,data,produce_pickle_files=PRODUCE_PICKLE_FILES):
+    def __init__(self, data, shuffle_data=True, Training=TRAINING):
         self.data = data
-        self.produce_pickle_files = produce_pickle_files
+
+        if shuffle_data:
+            self.data.sample(frac=1).reset_index(drop=True)
+
+        self.Training = Training
         self.fit_transform()
 
     def fit_transform(self):
         for item in preprocess_map.keys():
             operation = preprocess_map[item]
             if operation == "scale":
-                self.data[item] = self.__scale(self.data[item],item)
+                self.data[item] = self.__scale(self.data[item], item)
             elif operation == "LabelEncoder":
-                self.data[item] = self.__LabelEncoder(self.data[item],item)
-    
-    def __scale(self,data,col_name):
-        path = os.path.join(PICKLE_OBJ_PATH,col_name+".pkl")
-        if self.produce_pickle_files:
+                self.data[item] = self.__LabelEncoder(self.data[item], item)
+
+    def __scale(self, data, col_name):
+        path = os.path.join(PICKLE_OBJ_PATH, col_name+".pkl")
+        if self.Training:
             scaler = MinMaxScaler()
-            scaled_data = scaler.fit_transform(np.array(data).reshape(-1,1))
-            pickle.dump(scaler,open(path, 'wb'))
+            scaled_data = scaler.fit_transform(np.array(data).reshape(-1, 1))
+            pickle.dump(scaler, open(path, 'wb'))
         else:
             scaler = pickle.loads(path)
-            scaled_data = scaler.transform(np.array(data).reshape(-1,1))
+            scaled_data = scaler.transform(np.array(data).reshape(-1, 1))
         return scaled_data
 
-    def __LabelEncoder(self,data,col_name):
-        path = os.path.join(PICKLE_OBJ_PATH,col_name+".pkl")
-        if self.produce_pickle_files:
+    def __LabelEncoder(self, data, col_name):
+        path = os.path.join(PICKLE_OBJ_PATH, col_name+".pkl")
+        if self.Training:
             encoder = LabelEncoder()
             encoded_data = encoder.fit_transform(data)
-            pickle.dump(encoder,open(path, 'wb'))
+            pickle.dump(encoder, open(path, 'wb'))
         else:
             encoder = pickle.loads(path)
             encoded_data = encoder.transform(data)
 
         return encoded_data
+
+    def split_x_y(self):
+        self.y_data = self.data[LABEL]
+        self.x_data = self.data[self.data != self.data[LABEL]]
+        return self.x_data, self.y_data
+
+    def train_test_split(self, train_ratio=0.8):
+        self.split_x_y()
+        x_train_indx = int(train_ratio*len(self.x_data))
+        self.x_train = self.x_data.iloc[:x_train_indx, :]
+        self.y_train = self.y_data.iloc[:x_train_indx, :]
+
+        self.x_test = self.x_data.iloc[x_train_indx:, :]
+        self.y_test = self.y_data.iloc[x_train_indx:, :]
+
+        return self.x_train, self.y_train, self.x_test, self.y_test
+
+    def get_train_test_data(self):
+        self.train_test_split()
+        return self.x_train, self.y_train, self.x_test, self.y_test
 
     def get_data(self):
         return self.data
