3ef411cae186afd1a59ae28e495333cb17317100
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-11-14 18:40:42 +0200

    debuged preprocess

--- Utils/preprocess/preprocess.py
+++ Utils/preprocess/preprocess.py
@@ -3,12 +3,13 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 import pickle
 import config
+import pandas as pd
 # lat400sqkm	lon400sqkm	gear	year	kg	     rate	   species	 sid
 # Numirc        Numirc      Encode  Numirc  Numirc   Numirc    Encode    Encode
 PICKLE_OBJ_PATH = config.PICKLE_OBJ_PATH
 TRAINING = config.TRAINING
 LABEL = config.LABEL
-
+SCALE_LABEL = config.SCALE_LABEL
 preprocess_map = config.PREPROCESS_DICT
 
 class preprocess_fishiries():
@@ -25,6 +26,8 @@ class preprocess_fishiries():
         for item in preprocess_map.keys():
             operation = preprocess_map[item]
             if operation == "scale":
+                if not SCALE_LABEL and item in [LABEL]:
+                    continue
                 self.data[item] = self.__scale(self.data[item], item)
             elif operation == "LabelEncoder":
                 self.data[item] = self.__LabelEncoder(self.data[item], item)
@@ -54,17 +57,22 @@ class preprocess_fishiries():
 
     def split_x_y(self):
         self.y_data = self.data[LABEL]
-        self.x_data = self.data[self.data != self.data[LABEL]]
+        self.x_data = self.data.drop([LABEL], axis=1)
         return self.x_data, self.y_data
 
     def train_test_split(self, train_ratio=0.8):
         self.split_x_y()
         x_train_indx = int(train_ratio*len(self.x_data))
         self.x_train = self.x_data.iloc[:x_train_indx, :]
-        self.y_train = self.y_data.iloc[:x_train_indx, :]
+
+        if isinstance(LABEL,str):
+            self.y_train = self.y_data.iloc[:x_train_indx]
+            self.y_test = self.y_data.iloc[x_train_indx:]
+        else:
+            self.y_train = self.y_data.iloc[:x_train_indx, :]
+            self.y_test = self.y_data.iloc[x_train_indx:, :]
 
         self.x_test = self.x_data.iloc[x_train_indx:, :]
-        self.y_test = self.y_data.iloc[x_train_indx:, :]
 
         return self.x_train, self.y_train, self.x_test, self.y_test
 
